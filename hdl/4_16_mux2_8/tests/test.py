bbd9560c9e8de7258a3dfa6f3ee671d3fa1873bb
Debian <admin@ip-172-31-27-192.us-east-2.compute.internal>
2022-09-22 03:30:55 +0000

    made expensive test simpler

--- hdl/4_16_mux2_8/tests/test.py
+++ hdl/4_16_mux2_8/tests/test.py
@@ -6,17 +6,17 @@ from cocotb.types import Logic, LogicArray, Range
 
 @cocotb.test()
 async def test4_16(dut):
-    for d0Val in range(2**8):
-        for d1Val in range(2**8):
-            for sVal in range(2):
-               dut.d0.value = d0Val
-               dut.d1.value = d1Val
-               dut.s.value = sVal
-               # note that this logic has some delays in it
-               await Timer(2, units="ns")
-               y = model( \
-                   d0=LogicArray(d0Val, Range(7, 'downto', 0)), \
-                   d1=LogicArray(d1Val, Range(7, 'downto', 0)), \
-                   s=Logic(sVal) \
-               )
-               assert dut.y.value.binstr.upper() == y.binstr.upper(), f"HDL result incorrect: {dut.y.value.binstr.upper()} vs {y.binstr.upper()}"
+    for sVal in range(2):
+       d0Val = 1
+       d1Val = 2
+       dut.d0.value = d0Val
+       dut.d1.value = d1Val
+       dut.s.value = sVal
+       # note that this logic has some delays in it
+       await Timer(2, units="ns")
+       y = model( \
+           d0=LogicArray(d0Val, Range(7, 'downto', 0)), \
+           d1=LogicArray(d1Val, Range(7, 'downto', 0)), \
+           s=Logic(sVal) \
+       )
+       assert dut.y.value.binstr.upper() == y.binstr.upper(), f"HDL result incorrect: {dut.y.value.binstr.upper()} vs {y.binstr.upper()}"
